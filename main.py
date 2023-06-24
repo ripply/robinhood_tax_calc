07936e08d163042630585d075f5177983ffe50a4
vagdwd <vageesh.dwivedi@gmail.com>
2023-06-23 19:35:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,29 +3,33 @@
 from modules.database import establish_connection
 from modules.tax_calc import calculate_total_fees, calculate_total_investment, calculate_dividends_interest, calculate_stock_gains_and_losses, calculate_options_gains_and_losses
 
-TAX_YEAR = 2020
+def main(tax_year):
+    process_year(2018);
+    process_year(2019);
+    process_year(2020);
+    process_year(2021);
+    process_year(2022);
+    process_year(2023);
 
-
-def main():
+def process_year(tax_year)
     conn, cursor = establish_connection('transactions.sqlite')
 
-    fees_paid = calculate_total_fees(cursor, TAX_YEAR)
-    total_investment = calculate_total_investment(cursor, TAX_YEAR)
-    dividends_and_interest = calculate_dividends_interest(cursor, TAX_YEAR)
-    dividends_and_interest = calculate_dividends_interest(cursor, TAX_YEAR)
-    stock_gains_and_losses = calculate_stock_gains_and_losses(cursor, TAX_YEAR)
+    fees_paid = calculate_total_fees(cursor, tax_year)
+    total_investment = calculate_total_investment(cursor, tax_year)
+    dividends_and_interest = calculate_dividends_interest(cursor, tax_year)
+    dividends_and_interest = calculate_dividends_interest(cursor, tax_year)
+    stock_gains_and_losses = calculate_stock_gains_and_losses(cursor, tax_year)
     option_gains_and_losses = calculate_options_gains_and_losses(
-        cursor, TAX_YEAR)
+        cursor, tax_year)
     total_gains_and_losses = stock_gains_and_losses + option_gains_and_losses + dividends_and_interest
 
-    print(f"\nGains and losses for tax year {TAX_YEAR}")
+    print(f"\nGains and losses for tax year {tax_year}")
     print(f"Total fees paid to RH: ${fees_paid:.2f}")
     print(f"Total investment: ${total_investment:.2f}")
     print(f"Total dividends and interest: ${dividends_and_interest:.2f}")
     print(f"Total stock gains and losses: ${stock_gains_and_losses:.2f}")
     print(f"Total option gains and losses: ${option_gains_and_losses:.2f}")
     print(f"Total gains and losses: ${total_gains_and_losses:.2f}")
-
 
 if __name__ == "__main__":
     main()
