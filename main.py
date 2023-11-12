210f528ca024872ccac7a23bf0b559f57fdf697d
vagdwd <vageesh.dwivedi@gmail.com>
2023-11-12 13:20:47 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,13 @@
 from modules.database import establish_connection
 from modules.tax_calc import calculate_total_fees, calculate_total_investment, calculate_dividends_interest, calculate_stock_gains_and_losses, calculate_options_gains_and_losses
 
-def process_year(tax_year):
-    conn, cursor = establish_connection('transactions.sqlite')
-
-    normalize_splits(cursor, tax_year)
+def process_year(cursor, tax_year):
     fees_paid = calculate_total_fees(cursor, tax_year)
     total_investment = calculate_total_investment(cursor, tax_year)
     dividends_and_interest = calculate_dividends_interest(cursor, tax_year)
     dividends_and_interest = calculate_dividends_interest(cursor, tax_year)
     stock_gains_and_losses = calculate_stock_gains_and_losses(cursor, tax_year)
-    option_gains_and_losses = calculate_options_gains_and_losses(
-        cursor, tax_year)
+    option_gains_and_losses = calculate_options_gains_and_losses(cursor, tax_year)
     total_gains_and_losses = stock_gains_and_losses + option_gains_and_losses + dividends_and_interest
 
     print(f"\nGains and losses for tax year {tax_year}")
@@ -25,12 +21,15 @@ def process_year(tax_year):
     print(f"Total gains and losses: ${total_gains_and_losses:.2f}")
 
 def main():
-    process_year(2018);
-    process_year(2019);
-    process_year(2020);
-    process_year(2021);
-    process_year(2022);
-    process_year(2023);
+    conn, cursor = establish_connection('transactions.sqlite')
+    
+    normalize_splits(cursor)
+    process_year(cursor, 2018)
+    process_year(cursor, 2019)
+    process_year(cursor, 2020)
+    process_year(cursor, 2021)
+    process_year(cursor, 2022)
+    process_year(cursor, 2023)
 
 if __name__ == "__main__":
     main()
